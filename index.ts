f763ba03e4082e722c1247b5f25675124ef7504a
danielvanmil <dvanmil@gmail.com>
2023-03-20 15:10:15 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -29,18 +29,6 @@ var data1 = {
       x: DateTime.now().toISO(),
       y: 50,
     },
-    {
-      x: DateTime.now().toISO(),
-      y: 91,
-    },
-    {
-      x: DateTime.now().toISO(),
-      y: 200,
-    },
-    {
-      x: DateTime.now().toISO(),
-      y: 200,
-    },
   ],
 };
 
@@ -49,30 +37,7 @@ var data2 = {
   backgroundColor: 'red',
   data: [
     {
-      x: DateTime.now(),
-      y: 50,
-    },
-    {
-      x: DateTime.now(),
-      y: 91,
-    },
-    {
-      x: DateTime.now(),
-      y: 200,
-    },
-    {
-      x: DateTime.now(),
-      y: 200,
-    },
-  ],
-};
-
-var data3 = {
-  label: 's3',
-  backgroundColor: 'green',
-  data: [
-    {
-      x: DateTime.now(),
+      x: DateTime.now().plus({days: 10}).toISO(),
       y: 50,
     },
   ],
@@ -84,13 +49,13 @@ var ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext(
 new Chart<any>(ctx, {
   type: 'bar',
   data: {
-    datasets: [data1, data2, data3],
+    datasets: [data1, data2],
   },
   options: {
     scales: {
       x: {
-        min: DateTime.now().toISO(),
-        max: DateTime.now().toISO(),
+        min: DateTime.now().minus({days: 5}).toISO(),
+        max: DateTime.now().plus({days: 5}).toISO(),
         type: 'time',
         time: {
           unit: 'day',
